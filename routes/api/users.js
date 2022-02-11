9fd6a4fa591d39f47e2114d575cdae9ad8beedf5
Jaydeva Ahirrao <jaydeva.a.insnapsys@gmail.com>
2022-02-11 19:30:56 +0530

    INITIAL COMMIT

---
+++ routes/api/users.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+//User Schema model
+const User = require('../../models/Users.js');
+
+//@route  POST api/user
+//@desc   Register user
+//@access Public
+router.post(
+  '/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include the valid email').isEmail(),
+    check('password', 'please enter the password of min length 6').isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      //get user gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      //encrpt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //return json_web_token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+module.exports = router;

7dd8493c04ed367f2852608e6bad10a7c00b2c4f
Jaydeva Ahirrao <jaydeva.a.insnapsys@gmail.com>
2022-03-30 17:08:38 +0530

    dasd

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -36,7 +36,6 @@ function authReducer(state = initialState, action) {
         loading: false
       };
     case ACCOUNT_DELETED:
-    case AUTH_ERROR:
     case REGISTER_FAIL:
       localStorage.removeItem('token');
       return {
@@ -46,6 +45,7 @@ function authReducer(state = initialState, action) {
         loading: false
       };
     case LOGIN_FAIL:
+    case AUTH_ERROR:
     case LOGOUT:
       return {
         ...state,
